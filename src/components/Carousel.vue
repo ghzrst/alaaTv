b63bd6afbe524fc043c6422cd696d0c45d495fc0
Ghazaleh Rastegar <ghazaleh.rastegar74@gmail.com>
2020-09-09 23:31:58 +0430

    alaatv home page

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -1,49 +1,43 @@
 <template>
-  <div class="bd-example">
-    <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
-      <ol class="carousel-indicators">
-        <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
-        <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
-      </ol>
-      <div class="carousel-inner">
-        <div class="carousel-item active">
-          <img
-            src="https://cdn.alaatv.com/upload/images/slideShow/abrisham_slide_home2_20200906124025.jpg?w=965&amp;h=376"
-            class="d-block w-100"
-            alt="..."
-          />
-        </div>
-        <div class="carousel-item">
-          <img
-            src="https://cdn.alaatv.com/upload/images/slideShow/madrese_online_slide_20190902113043.jpg?w=965&amp;h=376"
-            class="d-block w-100"
-            alt="..."
-          />
-        </div>
-      </div>
-      <a
-        class="carousel-control-prev"
-        href="#carouselExampleCaptions"
-        role="button"
-        data-slide="prev"
-      >
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-      </a>
-      <a
-        class="carousel-control-next"
-        href="#carouselExampleCaptions"
-        role="button"
-        data-slide="next"
-      >
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-      </a>
-    </div>
+   <div class="mt-3 carousel-container">
+    <b-carousel
+      id="carousel-1"
+      v-model="slide"
+      :interval="4000"
+      controls
+      indicators
+      background="#ababab"
+      img-width="1024"
+      img-height="480"
+      style="text-shadow: 1px 1px 2px #333;"
+      @sliding-start="onSlideStart"
+      @sliding-end="onSlideEnd"
+    >
+      <b-carousel-slide
+        img-src="https://cdn.alaatv.com/upload/images/slideShow/abrisham_slide_home2_20200906124025.jpg?w=965&amp;h=376"
+      ></b-carousel-slide>
+
+      <b-carousel-slide img-src="https://cdn.alaatv.com/upload/images/slideShow/madrese_online_slide_20190902113043.jpg?w=965&amp;h=376">
+      </b-carousel-slide>
+    </b-carousel>
   </div>
 </template>
 <script>
 export default {
   name: "Carousel",
+  data() {
+      return {
+        slide: 0,
+        sliding: null
+      }
+    },
+    methods: {
+      onSlideStart() {
+        this.sliding = true
+      },
+      onSlideEnd() {
+        this.sliding = false
+      }
+    }
 };
 </script>
